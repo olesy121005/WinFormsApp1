a1cd20d43ee7846d1bd6d6ae512e7eed298c665c
malys <malys@OLESY>
2025-12-16 14:54:24 +0500

    Добавьте файлы проекта.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection.Emit;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace FormulaWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalc_Click(object sender, EventArgs e)
+        {
+            if (!double.TryParse(textBoxA.Text, out double a))
+            {
+                MessageBox.Show("Введите корректное число a");
+                return;
+            }
+
+            double z1 = 2 * Math.Pow(Math.Sin(3 * Math.PI - 2 * a), 2)
+                        * Math.Pow(Math.Cos(5 * Math.PI + 2 * a), 2);
+
+            double z2 = 0.25 - 0.25 * Math.Sin(5 * Math.PI / 2 - 8 * a);
+
+            labelZ1.Text = $"z1 = {z1:F6}";
+            labelZ2.Text = $"z2 = {z2:F6}";
+        }
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,50 @@
+﻿namespace FormulaWinForms
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Label labelA;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.Button buttonCalc;
+        private System.Windows.Forms.Label labelZ1;
+        private System.Windows.Forms.Label labelZ2;
+
+        private void InitializeComponent()
+        {
+            labelA = new System.Windows.Forms.Label();
+            textBoxA = new System.Windows.Forms.TextBox();
+            buttonCalc = new System.Windows.Forms.Button();
+            labelZ1 = new System.Windows.Forms.Label();
+            labelZ2 = new System.Windows.Forms.Label();
+
+            SuspendLayout();
+
+            labelA.AutoSize = true;
+            labelA.Location = new System.Drawing.Point(20, 20);
+            labelA.Text = "Введите a:";
+
+            textBoxA.Location = new System.Drawing.Point(120, 18);
+
+            buttonCalc.Location = new System.Drawing.Point(20, 60);
+            buttonCalc.Text = "Рассчитать";
+            buttonCalc.Click += buttonCalc_Click;
+
+            labelZ1.Location = new System.Drawing.Point(20, 110);
+            labelZ1.Text = "z1 =";
+
+            labelZ2.Location = new System.Drawing.Point(20, 140);
+            labelZ2.Text = "z2 =";
+
+            Controls.Add(labelA);
+            Controls.Add(textBoxA);
+            Controls.Add(buttonCalc);
+            Controls.Add(labelZ1);
+            Controls.Add(labelZ2);
+
+            Text = "Расчет по формулам";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+    }
+}
